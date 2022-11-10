08f616a4f593c0527a1a86e381bbfa4686f88cc7
reeko234 <jasonjoywinmorre@gmail.com>
2022-11-10 00:10:05 +0000

    Fresh start

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export function ProfileCard():React.ReactNode{
+    return <div>Profile Card</div>
+}
+
+// function expression
+export const OtherProfileCard:React.FC = () => <div>Another profile</div>
